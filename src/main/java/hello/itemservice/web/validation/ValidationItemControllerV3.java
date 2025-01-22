340b64cb92bb9363c3378ca85985cbe692f4e31f
minseok kyeon <msky0912@gmail.com>
2025-01-22 14:48:18 +0900

    Bean Validation - 오브젝트 오류
     - 오브젝트 오류같은 경우는 Bean Validation 보다는 자바코드로 처리

--- src/main/java/hello/itemservice/web/validation/ValidationItemControllerV3.java
+++ src/main/java/hello/itemservice/web/validation/ValidationItemControllerV3.java
@@ -48,6 +48,14 @@ public class ValidationItemControllerV3 {
     @PostMapping("/add")
     public String addItem(@Validated @ModelAttribute Item item, BindingResult bindingResult, RedirectAttributes redirectAttributes) {
 
+        //특정 필드가 아닌 복합 룰 검증
+        if (item.getPrice() != null && item.getQuantity() != null) {
+            int resultPrice = item.getPrice() * item.getQuantity();
+            if (resultPrice < 10000) {
+                bindingResult.reject("totalPriceMain", new Object[]{10000, resultPrice}, null);
+            }
+        }
+
         //검증에 실패하면 다시 입력 폼으로 -> 검증에서 오류가 1개라도 있으면 model에 errors를 담고 뷰 템플릿으로 보냄.
         if (bindingResult.hasErrors()) {
             log.info("errors={}", bindingResult);
