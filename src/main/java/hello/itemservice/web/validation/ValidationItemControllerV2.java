2f21cd7cb4489403aa403250b0e28f6b2c731ab5
minseok kyeon <msky0912@gmail.com>
2025-01-22 10:24:59 +0900

    Validator 분리 2
     - 스프링이 Validator 인터페이스를 제공하는 이유는 체계적 검증기능을 도입하기 위해서임. Validator 인터페이스를 사용해서 검증기를 만들면 스프링의 추가적인 도움을 받을 수 있음.
     - WebDataBinder는 스프링의 파라미터 바인딩의 역할을 해주고 검증 기능도 내부에 포함함.
     - 메소드에서 validator를 직접 호출하지않고 검증대상(target)앞에 @Validated를 붙이면 검증기를 실행하게 할 수 있음.
     - @Validated가 붙으면 WebDataBinder에 등록한 검증기를 찾아서 실행함. 어떤 검증기가 실행되어야할지 구분은 supports()가 처리하게 되고, supports(Item.class) 호출 이후 결과가 true면 ItemValidator의 validate()가 호출됨.
     - 검증시 @Validated, @Valid 둘다 사용가능함
     - @Valid를 사용하려면 build.gradle 의존관계 추가 필요
     - @Validated는 스프링 전용 검증 어노테이션이고, @Valid는 자바 표준 검증 어노테이션임.

--- src/main/java/hello/itemservice/web/validation/ValidationItemControllerV2.java
+++ src/main/java/hello/itemservice/web/validation/ValidationItemControllerV2.java
@@ -10,6 +10,8 @@ import org.springframework.util.StringUtils;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.validation.ObjectError;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -25,6 +27,14 @@ public class ValidationItemControllerV2 {
 
     private final ItemRepository itemRepository;
     private final ItemValidator itemValidator;
+
+    //WebDataBinder에 검증기를 추가하면 해당 컨트롤러에서는 검증기를 자동으로 적용할 수 있다.
+    //@InitBinder는 해당 컨트롤러에만 영향을 준다.
+    @InitBinder
+    public void init(WebDataBinder dataBinder) {
+        log.info("init binder {}", dataBinder);
+        dataBinder.addValidators(itemValidator);
+    }
 
     @GetMapping
     public String items(Model model) {
@@ -189,10 +199,26 @@ public class ValidationItemControllerV2 {
         return "redirect:/validation/v2/items/{itemId}";
     }
 
-    @PostMapping("/add")
+    //@PostMapping("/add")
     public String addItemV5(@ModelAttribute Item item, BindingResult bindingResult, RedirectAttributes redirectAttributes) {
 
         itemValidator.validate(item, bindingResult);
+
+        //검증에 실패하면 다시 입력 폼으로 -> 검증에서 오류가 1개라도 있으면 model에 errors를 담고 뷰 템플릿으로 보냄.
+        if (bindingResult.hasErrors()) {
+            log.info("errors={}", bindingResult);
+            return "validation/v2/addForm";
+        }
+
+        //성공 로직
+        Item savedItem = itemRepository.save(item);
+        redirectAttributes.addAttribute("itemId", savedItem.getId());
+        redirectAttributes.addAttribute("status", true);
+        return "redirect:/validation/v2/items/{itemId}";
+    }
+
+    @PostMapping("/add")
+    public String addItemV6(@Validated @ModelAttribute Item item, BindingResult bindingResult, RedirectAttributes redirectAttributes) {
 
         //검증에 실패하면 다시 입력 폼으로 -> 검증에서 오류가 1개라도 있으면 model에 errors를 담고 뷰 템플릿으로 보냄.
         if (bindingResult.hasErrors()) {
